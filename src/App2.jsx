4b61b7bcd09396aa0d317bb119c8de984a135a07
gowrivenneti <lakshmigowrivenneti@gmail.com>
2026-05-24 15:46:40 +0530

    the topics of hook and usestateare added with examples

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,11 +1,15 @@
 import Event from './Event.jsx'
 import Target from './Target.jsx'
+import Hook from './Hook.jsx'
+import Counter from './Counter.jsx'
 
 function App2() {
     return (
         <div>
             <Target />
             <Event />
+            <Hook />
+            <Counter />
         </div>
     )
 }
--- src/Hook.jsx
+++ src/Hook.jsx
@@ -1,7 +1,32 @@
+import React, { useState } from "react";
 function Hook(){
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
+    const [count, setCount] = useState(0);
+    const[isLoggedIn, setIsLoggedIn] = useState(false);
+    const updateName=()=>{
+        setName("Lucky");
+    }
+    const updateAge=()=>{
+        setAge(age+1);
+    }
+    const updateCount=()=>{
+        setCount(count+1);
+    }
+    const toggleLogin=()=>{
+        setIsLoggedIn(!isLoggedIn);
+    }
     return(
         <div>
-            <h1>Hook</h1>
+           <p>Welcome,{name}</p>
+            <button onClick={updateName}>Update Name</button>
+            <p>Age:{age}</p>
+            <button onClick={updateAge}>Update Age</button>
+            <p>Count:{count}</p>
+            <button onClick={updateCount}>Update Count</button>
+            <p>IsLoggedIn: {isLoggedIn ? "Logged In" : "Logged Out"}</p>
+            <button onClick={toggleLogin}>Toggle Login</button>
         </div>
     )
 }
+export default Hook;
